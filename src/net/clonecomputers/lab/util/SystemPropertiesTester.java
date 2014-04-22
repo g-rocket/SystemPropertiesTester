ebddf76b08a4440b6740934aedd1e74ec88d94b6
g_rocket <g.rocket1@gmail.com>
2014-04-22 02:03:23 -0400

    Add color!

--- src/net/clonecomputers/lab/util/SystemPropertiesTester.java
+++ src/net/clonecomputers/lab/util/SystemPropertiesTester.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import javax.swing.*;
+import javax.swing.text.*;
 
 import java.awt.*;
 
@@ -21,7 +22,6 @@ public class SystemPropertiesTester extends JPanel {
 	
 	public SystemPropertiesTester() {
 		Properties props = System.getProperties();
-		JTextArea text = new JTextArea(100,100);
 		List<Map.Entry<Object, Object>> propsList = new ArrayList<Map.Entry<Object, Object>>(props.entrySet());
 		Collections.sort(propsList, new Comparator<Map.Entry<Object,Object>>() {
 			@Override
@@ -30,18 +30,31 @@ public class SystemPropertiesTester extends JPanel {
 				return arg0.getKey().toString().compareTo(arg1.getKey().toString());
 			}
 		});
-		for(Map.Entry<Object, Object> p: propsList) {
-			text.append(p.getKey().toString());
-			text.append(": \u201C");
-			text.append(p.getValue().toString());
-			text.append("\u201D\n");
+		JTextPane textArea = new JTextPane();
+		StyledDocument document = textArea.getStyledDocument();
+		Style keyStyle = document.addStyle("keyStyle", null);
+		Style valueStyle = document.addStyle("valueStyle", null);
+		Style textStyle = document.addStyle("textStyle", null);
+		StyleConstants.setForeground(keyStyle, new Color(.3f,.5f,.1f));
+		StyleConstants.setForeground(valueStyle, new Color(.0f,.0f,.8f));
+		StyleConstants.setForeground(textStyle, Color.RED);
+		StyleConstants.setLineSpacing(textStyle, 2f);
+		try {
+			for(Map.Entry<Object, Object> p: propsList) {
+				document.insertString(document.getLength(), "[", textStyle);
+				document.insertString(document.getLength(), p.getKey().toString(), keyStyle);
+				document.insertString(document.getLength(), "] \u2192 [", textStyle);
+				document.insertString(document.getLength(), p.getValue().toString(), valueStyle);
+				document.insertString(document.getLength(), "]\n", textStyle);
+			}
+		} catch (BadLocationException e) {
+			throw new RuntimeException(e);
 		}
-		//text.setText(str.toString());
-		text.setEditable(false);
-		text.setLineWrap(false);
-		text.setVisible(true);
+		textArea.setEditable(false);
+		//textArea.setLineWrap(false);
+		textArea.setVisible(true);
 		this.setLayout(new BorderLayout());
-		this.add(new JScrollPane(text),BorderLayout.CENTER);
+		this.add(new JScrollPane(textArea),BorderLayout.CENTER);
 	}
 
 }
